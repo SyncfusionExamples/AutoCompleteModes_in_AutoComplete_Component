e54805bfb130aadd343acd6d2e39c01499b0bb0a
Keerthivasan Asokkumar <35622790+kaevee18@users.noreply.github.com>
2019-07-29 18:58:06 +0530

    Committed persistence and customization sample

---
+++ RichTextBox Integration/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApplication7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.autoComplete1 = new Syncfusion.Windows.Forms.Tools.AutoComplete(this.components);
+            this.richTextBox1 = new WindowsFormsApplication7.MyRichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.autoComplete1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // autoComplete1
+            // 
+            this.autoComplete1.HeaderFont = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World);
+            this.autoComplete1.ItemFont = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.autoComplete1.MetroColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(158)))), ((int)(((byte)(218)))));
+            this.autoComplete1.ParentForm = this;
+            this.autoComplete1.Style = Syncfusion.Windows.Forms.Tools.AutoCompleteStyle.Default;
+            this.autoComplete1.ThemeName = "Office2019Colorful";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(141, 133);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(207, 24);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.ForeColor = System.Drawing.Color.IndianRed;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(490, 349);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "RichTextBox Integration";
+            ((System.ComponentModel.ISupportInitialize)(this.autoComplete1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.Tools.AutoComplete autoComplete1;
+        private MyRichTextBox richTextBox1;
+    }
+}
+
